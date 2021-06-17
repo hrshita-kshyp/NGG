49200a3dd1fd7310cbae390a772fda65261f0416
Harshita <85073963+hrshita-kshyp@users.noreply.github.com>
2021-06-17 14:24:18 +0530

    Update Number_Guessing_Game.py

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -3,20 +3,16 @@ number = random.randint(1, 10)
 player_name = input("Hello, What's your name?")
 number_of_guesses=0
 
-print('okay! '+ player_name+ 'Guessing a number between 1 and 10:')
+print('okay! '+ player_name+'\t' 'Guess a number between 1 and 10:')
 while number_of_guesses < 5:
     guess = int(input())
     number_of_guesses += 1
     if guess < number:
         print('Your guess is too low')
-    if guess > number:
+    elif guess > number:
         print('Your guess is too high')
-    if guess == number:
-        break
-    if guess == number:
-      print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
     else:
-      print('You did not guess the number, The number was ' + str(number))
+        print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
 
 
 
